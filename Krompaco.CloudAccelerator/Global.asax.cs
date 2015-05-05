1dfa88a8bf4bd55ff4df33c5523806dd5113f49c
Johan Kronberg <kronberg@gmail.com>
2015-05-05 09:08:04 +0200

    Set DefaultConnectionLimit manually

--- Krompaco.CloudAccelerator/Global.asax.cs
+++ Krompaco.CloudAccelerator/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿namespace Krompaco.CloudAccelerator
 {
+    using System.Net;
     using System.Web.Mvc;
     using System.Web.Routing;
 
@@ -7,6 +8,10 @@
     {
         protected void Application_Start()
         {
+            // Indications that this might be a problem have been found, more info:
+            // https://social.msdn.microsoft.com/Forums/azure/en-US/d84ba34b-b0e0-4961-a167-bbe7618beb83/net-and-adonet-data-service-performance-tips-for-windows-azure-tables?forum=windowsazuredata
+            ServicePointManager.DefaultConnectionLimit = 512;
+
             MvcHandler.DisableMvcResponseHeader = true;
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
